3eba88c7217e58db23be5e786f2f01fb6e1eb6d1
Ashish9533 <ashish.kumar@rcvtechnologies.com>
2024-11-14 01:38:32 -0800

    Start new Resturent

---
+++ database/migrations/2024_11_12_124848_create_vendors_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('vendors', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->nullable();
+            $table->string('business_name')->nullable();
+            $table->string('account_holder_name')->nullable();
+            $table->string('account_number')->nullable();
+            $table->string('bank_name')->nullable();
+            $table->string('ifsc_code')->nullable();
+            $table->string('gstin')->nullable();
+            $table->string('pan')->nullable();
+        
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('vendors');
+    }
+};
